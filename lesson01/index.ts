600ee026a38ed052b61aa3616c0f63b54f030474
Ashik Mahmud <ashikmahmud137@gmail.com>
2021-09-11 19:23:01 +0600

    Initial Commit

---
+++ lesson01/index.ts
@@ -0,0 +1,43 @@
+import axios from 'axios';
+
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+// without typescript
+axios.get(url).then((response) => {
+  const todo = response.data;
+
+  const ID = todo.ID;
+  const title = todo.Title;
+  const finished = todo.finished;
+
+  console.log(`
+  The Todo with ID: ${ID}
+  Has a title of: ${title}
+  Is it finished: ${finished}
+  `);
+});
+
+// with typescript
+axios.get(url).then((response) => {
+  const todo = response.data as Todo;
+
+  const id = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
+
+  logTodo(id, title, completed);
+});
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+  console.log(`
+  The Todo with ID: ${id}
+  Has a title of: ${title}
+  Is it finished: ${completed}
+  `);
+};
