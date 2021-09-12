84f7b6d054f4479685fae79dcc5bf95a2c618b42
Ashik Mahmud <ashikmahmud137@gmail.com>
2021-09-12 16:11:07 +0600

    type system, different types

---
+++ 02 What is a Type System/types.ts
@@ -0,0 +1,15 @@
+const today = new Date();
+
+// we are getting all the properties and methods of the date object
+
+today.getMonth();
+
+const person = {
+  age: 20,
+};
+
+class Color {}
+
+// here red will have all the properties and methods of color
+
+const red = new Color();
